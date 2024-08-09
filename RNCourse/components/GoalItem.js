bcc669a70989b755701607e3aad33fff3fb70029
Saltypeesh <tungthanh254@gmail.com>
2024-08-10 02:22:49 +0700

    add GuessMyNumber layout and background image

---
+++ RNCourse/components/GoalItem.js
@@ -0,0 +1,33 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+function GoalItem(props) {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}  // Both Android and IOS
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+  goalText: {
+    padding: 8,
+    color: "white",
+  },
+});
